5417da847d9717d89cff467e91ed5e60b20cdeee
nawalassandh-ui <nawalassandh@gmail.com>
2025-11-30 13:51:20 +0530

    Update Day27.c

--- Day27.c
+++ Day27.c
@@ -1,1 +1,85 @@
+//Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
 
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+#include <stdio.h>
+void main() {
+    for (int i=1;i<=5;i++) {
+        for (int j=1;j<=(2*i)-1;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i=4;i>=1;i--) {
+        for (int j=(2*i)-1;j>=1;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+//Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include <stdio.h>
+void main() {
+    int n = 4;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
