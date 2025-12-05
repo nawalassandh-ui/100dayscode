c9fecc8d77cc4eb00a91c2f2fe5147857a65f9a9
nawalassandh-ui <nawalassandh@gmail.com>
2025-12-05 16:11:07 +0530

    Update Day13.c

--- Day13.c
+++ Day13.c
@@ -1,23 +1,4 @@
-//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
 
-/*
-Sample Test Cases:
-Input 1:
-4 2 +
-Output 1:
-6
-
-Input 2:
-10 3 %
-Output 2:
-1
-
-Input 3:
-15 5 /
-Output 3:
-3
-
-*/
 #include <stdio.h>
 
 void main() {
